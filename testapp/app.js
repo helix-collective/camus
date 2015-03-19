5d7e154a3743b910ca555a1324a1b9b508e22114
James Kozianski <soulplant@gmail.com>
2015-03-19 15:51:58 +0700

    Make testapp serve data/file at /file.

--- testapp/app.js
+++ testapp/app.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express')
 var app = express()
 
@@ -10,21 +11,30 @@ if (!(port >= 2000 && port <= 30000)) {
 app.get('/', function (req, res) {
   console.log('/');
   res.send('Hello World!')
-})
+});
 
 app.get('/status', function (req, res) {
   console.log('/status');
   res.send("I'm ok!");
-})
+});
 
 app.get('/status500', function (req, res) {
   console.log('/status500');
   res.status(500).send("I'm not OK!");
-})
+});
 
 app.get('/statusTimeout', function (req, res) {
   console.log('/statusTimeout - not going to send response');
-})
+});
+
+app.get('/file', function (req, res) {
+  try {
+    var data = fs.readFileSync('data/file');
+    res.send(data);
+  } catch (e) {
+    res.send('<no file>');
+  }
+});
 
 var server = app.listen(port, function () {
 
@@ -33,4 +43,4 @@ var server = app.listen(port, function () {
 
   console.log('Example app listening at http://%s:%s', host, port)
 
-})
+});
